8f0e18b3b731208db33301b688d49227c09387c8
Harsh8055git <harshsingh.eth@gmail.com>
2023-01-20 11:23:12 +0530

    aracna issue, stuck on loading

--- src/App.js
+++ src/App.js
@@ -1,78 +1,29 @@
-import { useEffect, useState } from 'react';
-import GoogleLogin from 'react-google-login';
-import { gapi } from "gapi-script";
-function App() {
-  const [values, setValue] = useState({});
-  const handleChange = (e) => {
-    setValue({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(values);
-  };
-  // password type change
-  const [passwordType, setPasswordType] = useState(false);
+import { Auth, useAuth } from "@arcana/auth-react";
 
-  const responseGoogle = (response) => {
-    console.log("Successfully Message:", response);
-  };
-  const resFailed = (res) => {
-    console.log("Failed:", res);
-  };
-  useEffect(()=>{
-    gapi.load("client:auth2", () => {
-      gapi.client.init({
-        clientId:
-          "703838845315-pupcra229gno6qnotq37tlj3vvq5l26l.apps.googleusercontent.com",
-        plugin_name: "faucets-app",
-      });
-    });
-  },[])
+const onLogin = () => {
+  // Route to authenticated page
+  console.log("u are logged in")
+}
+function App() {
+  const auth = useAuth();
   return (
-    <div className="auth_page_wrapper">
-        <div className="container">
-          <div className="auth_form">
-            <div className="title">
-              <h2>Sign Up</h2>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="form_group">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="form_group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type={passwordType ? "text" : "password"}
-                  name="password"
-                  placeholder="Enter your password"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="form_group">
-                <button type="submit">Sign Up</button>
-              </div>
-            </form>
-            <div className="auth_footer">
-              <GoogleLogin
-                clientId="703838845315-pupcra229gno6qnotq37tlj3vvq5l26l.apps.googleusercontent.com"
-                buttonText="Sign in with google"
-                onSuccess={responseGoogle}
-                onFailure={resFailed}
-                cookiePolicy={"single_host_origin"}
-              />
-            </div>
-          </div>
+    <div>
+      {auth.loading ? (
+        "Loading"
+      ) : auth.isLoggedIn ? (
+        <p>Logged In</p>
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"light"} onLogin={onLogin}/>
         </div>
-      </div>
+      )}
+
+      <div>
+          <Auth externalWallet={true} theme={"light"} onLogin={onLogin}/>
+        </div>
+    </div>
+
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "./_signup.scss";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+const appId = "a83863b141Be9968c478d61002cb7f75Cf116C6C";
+const provider = new AuthProvider(`${appId}`)
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProvideAuth provider={provider}>
+      <App />
+    </ProvideAuth>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
